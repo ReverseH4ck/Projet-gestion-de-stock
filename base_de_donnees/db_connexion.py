1c094a73a96685fc37092d0ce54c8df9696eee98
Moussa Tayaa <moussa.tayaa.dev@gmail.com>
2025-02-12 09:24:57 +0100

    deplacement du fichier main.py a la racine

--- base_de_donnees/db_connexion.py
+++ base_de_donnees/db_connexion.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
-import DatabaseUtils.base_de_donnees  # Importez votre classe
+import DatabaseUtils.base_de_donnees  # Importez la class
 
 
 try:
